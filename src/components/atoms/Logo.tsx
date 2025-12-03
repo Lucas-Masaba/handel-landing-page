5a1739ed9d10a71ba352f8e6b58021ac5b37a65b
Khusiima Luke Masaba <masabaluke@gmail.com>
2025-12-03 12:54:21 +0300

    fix: change logo link from anchor to div for improved semantics

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image';
 import handelLogo from '~/assets/images/handel-logo.svg';
 
 const Logo = () => (
-  <a href="/" className="inline-flex items-center gap-3 self-center ml-2 whitespace-nowrap">
+  <div className="inline-flex items-center gap-3 self-center ml-2 whitespace-nowrap">
     <Image src={handelLogo} alt="Handel logo" width={36} height={36} className="inline-block" />
     <span className="text-2xl font-bold text-[var(--brand-primary-600)] dark:text-white md:text-xl">Handel</span>
-  </a>
+  </div>
 );
 
 export default Logo;
